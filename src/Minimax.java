5481be8a76c5af4584a1979d3966a08b09329c4e
Jonathan Golden <jegolden@wpi.edu>
2023-09-17 14:06:03 -0400

    Changes by sarah

--- src/Minimax.java
+++ src/Minimax.java
@@ -55,47 +55,6 @@ public class Minimax {
             return new BestEdge(bestEdgeSoFar, min);
         }
     }
-    /*
-    public int minimaxDecision2(ArrayList<Edge> moves, Board board, Boolean isMaximizing, int max, int min, int depth){
-
-        if (depth == 0){
-            if (isMaximizing){
-                if (board.getScore() > max)
-                    max = board.getScore();
-                return max;
-            }
-            if (board.getScore() < min)
-                min = board.getScore();
-            return min;
-
-        }
-        // if steve (player1)
-        if(isMaximizing){
-            for (Edge move : moves) {
-                Board newBoard = makeMove(move.row1, move.col1, move.row2, move.col2, board);
-
-                int score = minimaxDecision(moves, newBoard, !isMaximizing, max, min, depth-1);
-
-                if (newBoard.getScore() > max)
-                    max = newBoard.getScore();
-                moves.remove(move);
-                ;
-                if(min <= max) { //TODO
-                    break;
-                }
-            }
-        }
-
-        // minimizing player
-        if(!isMaximizing){
-            for (Edge move : moves) {
-                Board newBoard = makeMove(move.row1, move.col1, move.row2, move.col2, board);
-                if (newBoard.getScore() < min)
-                    min = newBoard.getScore();
-            }
-        }
-        return "";
-    }*/
 
 
     /**
