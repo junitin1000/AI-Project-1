70b57ae4bed31b4a9dc32562559c948436bd7b31
Nick Borrello <nvborrello@wpi.edu>
2023-09-18 18:33:38 -0400

    steve listens for steve.go

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     static int boardSize = 2;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         Game game = new Game(boardSize, boardSize);
@@ -25,19 +28,32 @@ public class Main {
             gregTurn = new PlayerTurn(gameBoard, boardSize, "greg");
         }
 
-
-
-        if (steveGoesFirst()){
-            goAgain = steveTurn.takeTurn();
-            playerLastWent = "steve";
-        }
-        else{
-            goAgain = gregTurn.takeTurn();
-            playerLastWent = "greg";
-        }
         int counter = 1;
+        Path stevego = Paths.get(System.getProperty("user.dir") + "/src/steve.go");
+        Path stevepass = Paths.get(System.getProperty("user.dir") +"/src/steve.pass");
+        Path endgame = Paths.get(System.getProperty("user.dir") +"/src/end_game");
+        Path move_file = Paths.get(System.getProperty("user.dir") +"/src/move_file");
+        System.out.println(stevego);
         while (true) {
+            // check if the stevego file is in the directory and the moves file is empty
+            System.out.println(Files.exists(stevego));
+            if(!Files.exists(move_file)) {
+                System.out.println("Waiting for game to start");
+                continue;
+            } else if(Files.exists(stevego)) {
+                System.out.println("Steve's turn to go");
+            } else if (Files.exists(stevepass)) {
+                System.out.println("Other player's turn to go. Steve passed");
+            } else if (Files.exists(endgame)) {
+                System.out.println("Game over");
+                break;
+            } else {
+                System.out.println("Waiting for other player to go");
+                Thread.sleep(1000);
+                continue;
+            }
 
+            // If the game is over announce the winner
             if (counter == boardSize * (boardSize+1)*2) /*game is complete*/{
                 //Initiate ENDGAME
                 String winningPlayer;
@@ -51,28 +67,13 @@ public class Main {
                     System.out.println("GAME OVER: It's a tie!");
                     break;
                 }
-                System.out.println("GAME OVER: " + playerLastWent + " WINS!");
+                System.out.println("GAME OVER: " + " WINS!");
                 break;
             }
-            else if (goAgain){
-                if (playerLastWent.equals("steve")) {
-                    goAgain = steveTurn.takeTurn();
-                    playerLastWent = "steve";
-                }
-                else{
-                    goAgain = gregTurn.takeTurn();
-                    playerLastWent = "greg";
-                }
-            }
+            // Otherwise take steve's turn
             else{
-                if (playerLastWent.equals("steve")) {
-                    goAgain = gregTurn.takeTurn();
-                    playerLastWent = "greg";
-                }
-                else{
-                    goAgain = steveTurn.takeTurn();
-                    playerLastWent = "steve";
-                }
+                goAgain = steveTurn.takeTurn();
+                playerLastWent = "steve";
             }
             counter++;
         }
