29e73e4dd8507c06b448e0c9f727b2bb7acfa981
Jonathan Golden <jegolden@wpi.edu>
2023-09-19 21:30:27 -0400

    remade main for refMain and nonRefMain

--- src/Main.java
+++ src/Main.java
@@ -91,4 +91,76 @@ public class Main {
         return (goFirst == 0);
         //TODO integrate with referee later
     }
+
+    public static void nonRefMain(){
+        Scanner scan = new Scanner(System.in);
+
+        Game game = new Game(boardSize, boardSize);
+        game.printBoard();
+        String playerLastWent;
+        Board gameBoard = new Board();
+        boolean goAgain;
+        AITurn steveTurn = new AITurn(gameBoard, boardSize, "steve");
+        System.out.println("Do you want to play with AI vs. AI or Player vs. AI? (AI = 1, Player = 2)");
+        Turn gregTurn;
+        while (true) {
+
+            String AIorPlayer = scan.nextLine();
+
+            if (AIorPlayer.equals("1")) {
+                gregTurn = new AITurn(gameBoard, boardSize, "greg");
+                break;
+            } else if (AIorPlayer.equals("2")) {
+                gregTurn = new PlayerTurn(gameBoard, boardSize, "greg");
+                break;
+            } else {
+                System.out.println("Sorry, I didn't get that.");
+            }
+        }
+
+
+        if (steveGoesFirst()) {
+            goAgain = steveTurn.takeTurn();
+            playerLastWent = "steve";
+        } else {
+            goAgain = gregTurn.takeTurn();
+            playerLastWent = "greg";
+        }
+        int counter = 1;
+
+        while (true) {
+
+            if (counter == boardSize * (boardSize + 1) * 2) /*game is complete*/ {
+                //Initiate ENDGAME
+                String winningPlayer;
+                if (gameBoard.getScore("steve") > 0) {
+                    winningPlayer = "steve";
+                } else if (gameBoard.getScore("steve") < 0) {
+                    winningPlayer = "greg";
+                } else {
+                    System.out.println("GAME OVER: It's a tie!");
+                    break;
+                }
+                System.out.println("GAME OVER: " + winningPlayer + " WINS!");
+                break;
+            } else if (goAgain) {
+                if (playerLastWent.equals("steve")) {
+                    goAgain = steveTurn.takeTurn();
+                    playerLastWent = "steve";
+                } else {
+                    goAgain = gregTurn.takeTurn();
+                    playerLastWent = "greg";
+                }
+            } else {
+                if (playerLastWent.equals("steve")) {
+                    goAgain = gregTurn.takeTurn();
+                    playerLastWent = "greg";
+                } else {
+                    goAgain = steveTurn.takeTurn();
+                    playerLastWent = "steve";
+                }
+            }
+            counter++;
+        }
+    }
 }
