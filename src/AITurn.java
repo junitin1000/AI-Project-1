fa96d8e310a1d0cee92219e3c0b14cfb77e73353
sarahlarusso <sjlarusso@wpi.edu>
2023-09-24 17:51:51 -0400

    Changes by Jonathan Golden

--- src/AITurn.java
+++ src/AITurn.java
@@ -35,19 +35,23 @@ public class AITurn extends Turn {
         double start = System.currentTimeMillis();
         Edge decision = minimax.bestMove(gameBoard, name);
         double finish = System.currentTimeMillis();
-        String moveToMake = name + " " + decision.row1 + "," + decision.col1 + " " + decision.row2 + "," + decision.col2;
-        System.out.println("\tTime Taken: " + (finish - start) / 1000);
-        System.out.println(moveToMake.toUpperCase());
-        int[] numbers = getNums(moveToMake);
-        int r1 = numbers[0];
-        int c1 = numbers[1];
-        int r2 = numbers[2];
-        int c2 = numbers[3];
+        String moveToMake = "";
+        boolean complete = false;
+        //if (decision != null) {
+            moveToMake = name + " " + decision.row1 + "," + decision.col1 + " " + decision.row2 + "," + decision.col2;
 
-        //Update real board
-        boolean complete = updateBoard(r1, c1, r2, c2, gameBoard, name, true);
-        gameBoard.edges.remove(decision);
+            System.out.println("\tTime Taken: " + (finish - start) / 1000);
+            System.out.println(moveToMake.toUpperCase());
+            int[] numbers = getNums(moveToMake);
+            int r1 = numbers[0];
+            int c1 = numbers[1];
+            int r2 = numbers[2];
+            int c2 = numbers[3];
 
+            //Update real board
+            complete = updateBoard(r1, c1, r2, c2, gameBoard, name, true);
+            gameBoard.edges.remove(decision);
+        //}
         // Write the move to the move file
         Path move_file = Paths.get(System.getProperty("user.dir") + "/src/dots_boxes_referee/move_file");
         try {
